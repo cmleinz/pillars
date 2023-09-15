2d3554f00c603fe0f8a6eb49642e4bc28e19ad05
Caleb Leinz <cleinz+github@pm.me>
2023-09-14 17:58:42 -0700

    Basic premise

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Pillars
+
+**DO NOT USE THIS LIBRARY** :)
+
+I don't know enough unsafe rust to know exactly why this library is unsound, but
+I'm CONFIDENT there are LOTS of unconsidered aspects to this. I'm mostly using
+it to learn and welcome any PRs which solve the spirit of the problem in a sound
+way.
+
+The idea of this library is to use a message passing system for tokio
+tasks which routes messages amongst tasks based solely on the type of 
+message being sent. This allows a task to send a message of type `T` and have
+that message routed to running task which is listening for messages of type `T`.
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,111 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{any::TypeId, collections::HashMap, sync::OnceLock};
+
+use tokio::sync::mpsc;
+
+static BROKER: OnceLock<mpsc::Sender<RawMessage>> = OnceLock::new();
+
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum Error {
+    #[error("There is no receiver of this currently held by the Broker")]
+    NoReceiver,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct Wrapper<P> {
+    receiver: mpsc::Receiver<RawMessage>,
+    inner: P,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<P> Wrapper<P>
+where
+    P: PillarExt,
+{
+    fn recv(&mut self, message: RawMessage) {
+        let ptr = message.ptr as *mut <P as Pillar>::Message;
+        let message = unsafe { Box::from_raw(ptr) };
+        self.inner.recv(message).unwrap();
+    }
+
+    async fn spawn(mut self) {
+        loop {
+            let msg = self.receiver.recv().await.unwrap();
+            self.recv(msg);
+        }
+    }
+}
+
+struct RawMessage {
+    id: TypeId,
+    ptr: *mut usize,
+}
+
+unsafe impl Send for RawMessage {}
+
+pub trait Message {
+    type Response;
+}
+
+pub trait Pillar: 'static + Send {
+    type Message: 'static + Send + Message;
+
+    type Error;
+
+    fn recv(
+        &mut self,
+        message: Box<Self::Message>,
+    ) -> Result<<Self::Message as Message>::Response, Error>;
+
+    fn spawn(self) -> Result<(), Self::Error>;
+}
+
+// Should prevent send from ever being reimplemented
+impl<P: Pillar> PillarExt for P {}
+
+pub trait PillarExt: Pillar {
+    fn send<M>(&self, message: M) -> Result<<Self::Message as Message>::Response, Error>
+    where
+        M: 'static + Send + Message,
+    {
+        let leaked_message = Box::leak(Box::new(message));
+        let invalid_message_ptr = leaked_message as *mut M as *mut usize;
+        let message = RawMessage {
+            id: TypeId::of::<M>(),
+            ptr: invalid_message_ptr,
+        };
+
+        let global_sender = BROKER.get().unwrap();
+        global_sender.blocking_send(message).unwrap();
+
+        todo!()
+    }
+}
+
+pub struct Broker {
+    receiver: mpsc::Receiver<RawMessage>,
+    pillars: HashMap<TypeId, mpsc::Sender<RawMessage>>,
+}
+
+impl Broker {
+    async fn forward(&self, message: RawMessage) -> Result<(), Error> {
+        let Some(sender) = self.pillars.get(&message.id) else {
+            return Err(Error::NoReceiver);
+        };
+
+        if let Err(_) = sender.send(message).await {
+            return Err(Error::NoReceiver);
+        }
+
+        Ok(())
+    }
+
+    pub fn add_pillar<P: Pillar>(&mut self, pillar: P) {
+        let (tx, rx) = mpsc::channel(100);
+        let id = TypeId::of::<<P as Pillar>::Message>();
+        let wrapper = Wrapper {
+            receiver: rx,
+            inner: pillar,
+        };
+
+        self.pillars.insert(id, tx);
+        tokio::spawn(async move { wrapper.spawn().await });
     }
 }
